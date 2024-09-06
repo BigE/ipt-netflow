043f2fb0611fa19db7b3e26e8d2e06bb02f7ecf7
Eric Gach <eric.gach@gmail.com>
2024-09-06 12:51:49 -0500

    Updating for removal of prandom_u32_below

--- ipt_NETFLOW.c
+++ ipt_NETFLOW.c
@@ -4454,7 +4454,7 @@ static int netflow_scan_and_export(const int flush)
 				val = nf->sampler_count % interval;
 				break;
 			case SAMPLER_RANDOM:
-				val = prandom_u32_max(interval);
+				val = get_random_u32_below(interval);
 				break;
 			default: /* SAMPLER_HASH */
 				val = 0;
@@ -5719,7 +5719,7 @@ static int __init ipt_netflow_init(void)
 #endif
 
 #ifdef ENABLE_RANDOM_TEMPLATE_IDS
-	template_ids = FLOWSET_DATA_FIRST | prandom_u32_max(0x00010000);
+	template_ids = FLOWSET_DATA_FIRST | get_random_u32_below(0x00010000);
 #endif
 
 #ifdef SNMP_RULES
